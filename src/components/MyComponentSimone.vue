6d913000655f63ff0ada68810303eefab29d275f
Simone Confalonieri <s.confalonieri@campus.unimib.it>
2023-01-17 17:58:15 +0100

    Creation MyComponent

---
+++ src/components/MyComponentSimone.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Simone</h1>
+    <p>https://github.com/SimoneConfalonieri8</p>
+  </div>
+</template>
+<script setup lang="ts">
+    
+</script>
+<style scoped></style>
